c1ba73c29e94187aa5c78e4f12333f768e04d40f
Mounir Ybanez <rinuom91@gmail.com>
2014-12-17 18:01:56 +0800

    Added tests on room name.

--- server/create_room_handler_test.go
+++ server/create_room_handler_test.go
@@ -40,7 +40,7 @@ func TestCreateRoomHandlerUsernameErrorMessageNotSetAsParameter(t *testing.T) {
 	// Creating parameter values for the request
 	params := url.Values{}
 	// Creating an http request
-	request, _ := http.NewRequest("POST", "localhost/8080/chat/room/create", bytes.NewBufferString(params.Encode()))
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/create", bytes.NewBufferString(params.Encode()))
 	// Creating a response recorder
 	w := httptest.NewRecorder()
 	// Serving the http request and the response recorder
@@ -54,5 +54,53 @@ func TestCreateRoomHandlerUsernameErrorMessageNotSetAsParameter(t *testing.T) {
 	// Checking if the username key exist in the response
 	if _, ok := r["username"]; !ok {
 		t.Error("There's no username error message.")
+	}
+}
+
+func TestCreateRoomHandlerRoomNameErrorMessageWithEmptyParamValue(t *testing.T) {
+	// Creating the handler function for the create room handler
+	createRoomHandler := createRoomHandler()
+	// Creating parameter values for the request
+	params := url.Values{}
+	// Adding a parameter
+	params.Add("username", "")
+	// Creating an http request
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/create", bytes.NewBufferString(params.Encode()))
+	// Creating a response recorder
+	w := httptest.NewRecorder()
+	// Serving the http request and the response recorder
+	createRoomHandler.ServeHTTP(w, request)
+	// Declaring a container for the response
+	var response interface{}
+	// Converting json to response container
+	json.Unmarshal(w.Body.Bytes(), &response)
+	// Typecasting the response container to map
+	r := response.(map[string]interface{})
+	// Checking if the room_name key exist in the response
+	if _, ok := r["room_name"]; !ok {
+		t.Error("There's no room_name error message.")
+	}
+}
+
+func TestCreateRoomHandlerRoomNameErrorMessageNotSetAsParameter(t *testing.T) {
+	// Creating the handler function for the create room handler
+	createRoomHandler := createRoomHandler()
+	// Creating parameter values for the request
+	params := url.Values{}
+	// Creating an http request
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/create", bytes.NewBufferString(params.Encode()))
+	// Creating a response recorder
+	w := httptest.NewRecorder()
+	// Serving the http request and the response recorder
+	createRoomHandler.ServeHTTP(w, request)
+	// Declaring a container for the response
+	var response interface{}
+	// Converting json to response container
+	json.Unmarshal(w.Body.Bytes(), &response)
+	// Typecasting the response container to map
+	r := response.(map[string]interface{})
+	// Checking if the room_name key exist in the response
+	if _, ok := r["room_name"]; !ok {
+		t.Error("There's no room name error message.")
 	}
 }
