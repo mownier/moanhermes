366de130af362f46fa759455fccc00232d9f1dae
Mounir Ybanez <rinuom91@gmail.com>
2014-12-18 16:08:33 +0800

    Added tests on empty username and room_id parameters, response content type, error messages.

---
+++ server/join_room_handler_test.go
@@ -0,0 +1,110 @@
+package server
+
+import (
+	"testing"
+	"net/url"
+	"net/http"
+	"net/http/httptest"
+	"bytes"
+	"encoding/json"
+)
+
+func TestJoinRoomHandlerShouldRespondWithErrorMessageIfRoomIdNotSet(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/join", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+	if _, ok := r["room_id"]; !ok {
+		t.Error("Should respond with an error message if room id is not set.")
+	} else if r["room_id"] != "Room id is required." {
+		t.Error("Room id error message should be 'Room id is required.'")
+	}
+}
+
+func TestJoinRoomHandlerShouldRespondWithErrorMessageIfEmptyRoomIdParameter(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	params.Add("room_id", "")
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/join", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+	if _, ok := r["room_id"]; !ok {
+		t.Error("Should respond with an error message if room id is not set.")
+	} else if r["room_id"] != "Room id is required." {
+		t.Error("Room id error message should be 'Room id is required.'")
+	}
+}
+
+func TestJoinRoomHandlerShouldRespondWithErrorMessageIfUsernameNotSet(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/join", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+	if _, ok := r["username"]; !ok {
+		t.Error("Should respond with an error message if username is not set.")
+	} else if r["username"] != "Username is required." {
+		t.Error("Username error message should be 'Username is required.'")
+	}
+}
+
+func TestJoinRoomHandlerShouldRespondWithErrorMessageIfEmptyUsernameParameter(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	params.Add("username", "")
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/join", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+	if _, ok := r["username"]; !ok {
+		t.Error("Should respond with an error message if username is not set.")
+	} else if r["username"] != "Username is required." {
+		t.Error("Username error message should be 'Username is required.'")
+	}
+}
+
+func TestJoinRoomHandlerShouldRespondWithErroMessageIfRequestMethodIsNotPOST(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	// GET, PUT, DELETE
+	request, _ := http.NewRequest("GET", "localhost:8080/chat/room/join", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+	if _, ok := r["message"]; !ok {
+		t.Error("Should respond with an error message.")
+	} else if r["message"] != "Method not allowed." {
+		t.Error("Error message content should be 'Method not allowed.'")
+	}
+}
+
+func TestJoinRoomHandlerResponseContentType(t *testing.T) {
+	joinRoomHandler := joinRoomHandler()
+	params := url.Values{}
+	request, _ := http.NewRequest("POST", "localhost:8080/chat/room", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	joinRoomHandler.ServeHTTP(w, request)
+	if w.Header().Get("Content-Type") != "application/json" {
+		t.Error("Content-Type of the response should be 'application/json'.")
+	}
+}
