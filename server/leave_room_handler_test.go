f33a2b0962eb209cbeb60f5b83d4f65a5a4b1f3f
Mounir Ybanez <rinuom91@gmail.com>
2014-12-19 11:30:52 +0800

    Added test on empty username.

--- server/leave_room_handler_test.go
+++ server/leave_room_handler_test.go
@@ -16,7 +16,7 @@ func TestLeaveRoomHandlerIfNotMethodDELETE(t *testing.T) {
 	room.Users = append(room.Users, member)
 	rooms = append(rooms, room)
 
-	leaveRooomHandler := leaveRoomHandler()
+	leaveRoomHandler := leaveRoomHandler()
 	params := url.Values{}
 	params.Add("username", member.Username)
 	params.Add("room_id", room.Uid)
@@ -24,7 +24,7 @@ func TestLeaveRoomHandlerIfNotMethodDELETE(t *testing.T) {
 	request, _ := http.NewRequest("POST", "localhost:8080/chat/room/leave", bytes.NewBufferString(params.Encode()))
 	request.Header.Set("Content-Type", "application/x-www-urlencoded; param=value")
 	w := httptest.NewRecorder()
-	leaveRooomHandler.ServeHTTP(w, request)
+	leaveRoomHandler.ServeHTTP(w, request)
 	
 	var response interface{}
 	json.Unmarshal(w.Body.Bytes(), &response)
@@ -36,5 +36,26 @@ func TestLeaveRoomHandlerIfNotMethodDELETE(t *testing.T) {
 		t.Error("There is no message key.")
 	} else if r["message"] != "Method not allowed." {
 		t.Error("Error message should be 'Method not allowed.'")
+	}
+}
+
+func TestLeaveRoomHandlerEmptyUsernameParameterValue(t *testing.T) {
+	leaveRoomHandler := leaveRoomHandler()
+	params := url.Values{}
+	request, _ := http.NewRequest("DELETE", "localhost:8080/chat/room/leave", bytes.NewBufferString(params.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-urlencoded; param=value")
+	w := httptest.NewRecorder()
+	leaveRoomHandler.ServeHTTP(w, request)
+
+	var response interface{}
+	json.Unmarshal(w.Body.Bytes(), &response)
+	r := response.(map[string]interface{})
+
+	if w.Code != http.StatusBadRequest {
+		t.Error("Status code should be http.StatusBadRequest.")
+	} else if _, ok := r["username"]; !ok {
+		t.Error("There is no message key.")
+	} else if r["username"] != "Username is required." {
+		t.Error("Error message should be 'Username is required.'")
 	}
 }
