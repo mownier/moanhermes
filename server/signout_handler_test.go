59c2545dca348152d644a339f3079ccd69c35ec8
Mounir Ybanez <rinuom91@gmail.com>
2014-12-23 09:43:56 +0800

    Added file.

---
+++ server/signout_handler_test.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"encoding/json"
+	"bytes"
+)
+
+func TestSignoutHandlerInvalidRequestMethod(t *testing.T) {
+	
+}
